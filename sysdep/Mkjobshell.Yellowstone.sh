38f751cb3359c83c80d17b0b42d1533be0bbc351
Ryuji Yoshida <ryoshida@riken.jp>
2016-06-01 19:10:57 +0900

    add a ico2ll script for netcdf output with DCMIP CF-compliant.

--- sysdep/Mkjobshell.Yellowstone.sh
+++ sysdep/Mkjobshell.Yellowstone.sh
@@ -52,7 +52,19 @@ cat << EOF1 > run.sh
  
 export OMP_NUM_THREADS=4
 export MP_TASK_AFFINITY=core:$OMP_NUM_THREADS
- 
+
+ln -sv ${TOPDIR}/bin/${BINNAME} .
+ln -sv ${TOPDIR}/data/mnginfo/${MNGINFO} .
+ln -sv ${TOPDIR}/data/grid/vgrid/${VGRID} .
+EOF1
+
+for f in $( ls ${TOPDIR}/data/grid/boundary/${dir2d} )
+do
+   echo "ln -sv ${TOPDIR}/data/grid/boundary/${dir2d}/${f} ." >> run.sh
+done
+
+cat << EOF2 >> run.sh
+
 ${MPIEXEC} ./${BINNAME}
 
 ################################################################################
@@ -79,6 +91,18 @@ cat << EOFICO2LL1 > ico2ll.sh
 export OMP_NUM_THREADS=4
 export MP_TASK_AFFINITY=core:$OMP_NUM_THREADS
 
+ln -sv ${TOPDIR}/bin/fio_ico2ll_mpi .
+ln -sv ${TOPDIR}/data/mnginfo/${MNGINFO} .
+ln -sv ${TOPDIR}/data/zaxis .
+EOFICO2LL1
+
+for f in $( ls ${TOPDIR}/data/grid/llmap/gl${GL}/rl${RL}/ )
+do
+   echo "ln -sv ${TOPDIR}/data/grid/llmap/gl${GL}/rl${RL}/${f} ." >> ico2ll.sh
+done
+
+cat << EOFICO2LL2 >> ico2ll.sh
+
 # run
 ${MPIEXEC} ./fio_ico2ll_mpi \
 history \
@@ -92,4 +116,135 @@ outfile_dir="../" \
 -comm_smallchunk
 
 ################################################################################
-EOFICO2LL1
+EOFICO2LL2
+
+
+cat << EOFICO2LLNC1 > ico2ll_netcdf.sh
+#! /bin/bash -x
+################################################################################
+#
+# for NCAR Yellowstone (IBM iDataPlex Sandybridge)
+#
+################################################################################
+#BSUB -a poe                  # set parallel operating environment
+#BSUB -P ${prjcode}           # project code
+#BSUB -J ico2ll               # job name
+#BSUB -W 00:10                # wall-clock time (hrs:mins)
+#BSUB -n ${NMPI}              # number of tasks in job
+#BSUB -R "span[ptile=4]"      # run four MPI tasks per node
+#BSUB -q regular              # queue
+#BSUB -e errors.%J.ico2ll     # error file name in which %J is replaced by the job ID
+#BSUB -o output.%J.ico2ll     # output file name in which %J is replaced by the job ID
+
+export OMP_NUM_THREADS=4
+export MP_TASK_AFFINITY=core:$OMP_NUM_THREADS
+
+# User Settings
+# ------------------------------------------------------------------------------
+
+glev=5          # g-level of original grid
+case=163        # test case number
+out_intev='1hr' # output interval (format: "1hr", "6hr", "day", "100s")
+
+# ------------------------------------------------------------------------------
+
+ln -sv ${TOPDIR}/bin/fio_ico2ll_mpi .
+ln -sv ${TOPDIR}/data/mnginfo/${MNGINFO} .
+ln -sv ${TOPDIR}/data/zaxis .
+EOFICO2LLNC1
+
+for f in $( ls ${TOPDIR}/data/grid/llmap/gl${GL}/rl${RL}/ )
+do
+   echo "ln -sv ${TOPDIR}/data/grid/llmap/gl${GL}/rl${RL}/${f} ." >> ico2ll_netcdf.sh
+done
+
+cat << EOFICO2LLNC2 >> ico2ll_netcdf.sh
+
+# run
+${MPIEXEC} ./fio_ico2ll_mpi \
+history \
+glevel=${GLEV} \
+rlevel=${RLEV} \
+mnginfo="./${MNGINFO}" \
+layerfile_dir="./zaxis" \
+llmap_base="./llmap" \
+output_netcdf=.true. \
+dcmip2016=.true. \
+-lon_swap \
+-comm_smallchunk
+
+# netcdf combine
+case \${glev} in
+5)
+ reso='r200'
+;;
+6)
+ reso='r100'
+;;
+7)
+ reso='r50'
+;;
+8)
+ reso='r25'
+;;
+esac
+
+case \${case} in
+161) # Moist baroclinic wave
+levs='L30'
+target=( u.nc v.nc w.nc prs.nc t.nc qv.nc qc.nc qr.nc pasv1.nc pasv2.nc \
+u850.nc v850.nc w850.nc t850.nc w500.nc t500.nc ps.nc prcp.nc \
+forcing_vx.nc forcing_vy.nc forcing_vz.nc forcing_e.nc \
+forcing_qv.nc forcing_qc.nc forcing_qr.nc forcing_cl.nc forcing_cl2.nc \
+cl_column.nc cl2_column.nc cly_column.nc )
+;;
+162) # Idealized tropical cyclone
+levs='L30'
+target=( u.nc v.nc w.nc prs.nc t.nc qv.nc qc.nc qr.nc \
+u850.nc v850.nc w850.nc t850.nc w500.nc t500.nc ps.nc prcp.nc \
+forcing_vx.nc forcing_vy.nc forcing_vz.nc forcing_e.nc \
+forcing_qv.nc forcing_qc.nc forcing_qr.nc )
+;;
+163) # Supercell
+levs='L40'
+target=( u.nc v.nc w.nc prs.nc t.nc qv.nc qc.nc qr.nc \
+u850.nc v850.nc w850.nc t850.nc w500.nc t500.nc ps.nc prcp.nc \
+forcing_vx.nc forcing_vy.nc forcing_vz.nc forcing_e.nc \
+forcing_qv.nc forcing_qc.nc forcing_qr.nc )
+;;
+esac
+
+model='nicam'
+org_grid='hex'
+dat_grid='interp_latlon'
+equation='nonhydro'
+description='g-level_'\${glev}
+
+fname=\${model}.\${case}.\${reso}.\${levs}.\${dat_grid}.\${equation}.nc
+
+input=""
+for (( i=0; i <\${#target[@]}; ++i ))
+do
+input=\$input" "\${target[\$i]}
+done
+
+echo "start cdo process"
+cdo -s merge \${input}                                           temporary_A.nc
+cdo -s setgatt,model,\${model}                                   temporary_A.nc temporary_B.nc
+rm temporary_A.nc; cdo -s setgatt,test_case,\${case}             temporary_B.nc temporary_A.nc
+rm temporary_B.nc; cdo -s setgatt,horizontal_resolution,\${reso} temporary_A.nc temporary_B.nc
+rm temporary_A.nc; cdo -s setgatt,levels,\${levs}                temporary_B.nc temporary_A.nc
+rm temporary_B.nc; cdo -s setgatt,grid,\${dat_grid}              temporary_A.nc temporary_B.nc
+rm temporary_A.nc; cdo -s setgatt,equation,\${equation}          temporary_B.nc temporary_A.nc
+rm temporary_B.nc; cdo -s setgatt,time_frequency,\${out_intev}   temporary_A.nc temporary_B.nc
+rm temporary_A.nc; cdo -s setgatt,description,\${description}    temporary_B.nc temporary_A.nc
+cdo -s setgatt,history,dcmip2016                                temporary_A.nc \${fname}
+
+for (( i=0; i <\${#target[@]}; ++i ))
+do
+ rm \${target[\$i]}
+done
+rm temporary_A.nc temporary_B.nc
+
+################################################################################
+EOFICO2LLNC2
